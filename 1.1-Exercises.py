c6dff336660572b2c1259f3b21c97f15ee9470e9
iorala <andreas.laroi@gmail.com>
2021-09-25 15:33:01 +0200

    Rename Files

---
+++ 1.1-Exercises.py
@@ -0,0 +1,11 @@
+#Addieren Sie alle Zahlen von 1 bis 5 miteinander
+print(1+2+3+4+5);
+
+#Subtrahieren Sie allen Zahlen von 1 bis 5 voneinander
+print(1-2-3-4-5);
+
+#Multiplizieren Sie alle Zahlen von 1 bis 5 miteinander
+print(1*2*3*4*5);
+
+#Dividieren Sie alle Zahlen von 1 bis 5 durcheinander
+print(1/2/3/4/5);
